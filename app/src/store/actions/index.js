bf3d310eadd313e081fd72c861a4ffbab931dc18
Anthony N <foto.arn@gmail.com>
2020-06-10 21:57:22 -0700

    action created

--- app/src/store/actions/index.js
+++ app/src/store/actions/index.js
@@ -4,14 +4,14 @@ export const FETCHING_AMIIBO_START = "FETCHING_AMIIBO_START";
 export const FETCHING_AMIIBO_SUCCESS = "FETCHING_AMIIBO_SUCCESS";
 export const FETCHING_AMIIBO_FAILURE = "FETCHING_AMIIBO_FAILURE";
 
-export const getAmiibo = (url) => (dispach) => {
-  dispatchEvent({ type: FETCHING_AMIIBO_START });
+export const getAmiibo = (url) => (dispatch) => {
+  dispatch({ type: FETCHING_AMIIBO_START });
   axios
     .get(url)
     .then((res) => {
-      dispatchEvent({ type: FETCHING_AMIIBO_SUCCESS, payload: res.data });
+      dispatch({ type: FETCHING_AMIIBO_SUCCESS, payload: res.data });
     })
     .catch((err) => {
-      dispatchEvent({ type: FETCHING_AMIIBO_FAILURE });
+      dispatch({ type: FETCHING_AMIIBO_FAILURE });
     });
 };
